5cf3ddd6110fa6ccc3d5115d05099943fb8ef30f
Cameron11010 <115788189+Cameron11010@users.noreply.github.com>
2026-04-11 15:52:37 +0100

    Initialize Baseball Tracker application structure

---
+++ Baseball Tracker/Baseball_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baseball_TrackerApp.swift
+//  Baseball Tracker
+//
+//  Created by Cameron Millar on 24/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Baseball_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
